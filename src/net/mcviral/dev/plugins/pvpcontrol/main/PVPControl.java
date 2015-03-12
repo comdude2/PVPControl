935e83993e026cbf51b8f8a45a52e59db529befe
Matt <Matt@Matt-Desktop-PC>
2015-03-12 01:37:00 +0000

    First commit.

---
+++ src/net/mcviral/dev/plugins/pvpcontrol/main/PVPControl.java
@@ -0,0 +1,17 @@
+package net.mcviral.dev.plugins.pvpcontrol.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PVPControl extends JavaPlugin{
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+	
+	
+}
---
+++ src/net/mcviral/dev/plugins/pvpcontrol/pvp/Member.java
@@ -0,0 +1,15 @@
+package net.mcviral.dev.plugins.pvpcontrol.pvp;
+
+import java.util.UUID;
+
+public class Member {
+	
+	private UUID uuid = null;
+	private String name = null;
+	private boolean globalpvp = true;
+	
+	public Member(){
+		
+	}
+	
+}
