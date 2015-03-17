ab42a81f1456a1a617f6ffe6d86b6102e1159e7b
Matt <Matt@Matt-Desktop-PC>
2015-03-17 06:05:45 +0000

    some changes

--- src/net/mcviral/dev/plugins/pvpcontrol/gangs/territory/Territory.java
+++ src/net/mcviral/dev/plugins/pvpcontrol/gangs/territory/Territory.java
@@ -1,5 +1,30 @@
 package net.mcviral.dev.plugins.pvpcontrol.gangs.territory;
 
 public class Territory {
+	
+	private String name = null;
+	private String gangincontrol = null;
+	
+	public Territory(){
+		
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGangincontrol() {
+		return gangincontrol;
+	}
+
+	public void setGangincontrol(String gangincontrol) {
+		this.gangincontrol = gangincontrol;
+	}
+	
+	
+	
 }
--- src/net/mcviral/dev/plugins/pvpcontrol/gangs/territory/TerritoryController.java
+++ src/net/mcviral/dev/plugins/pvpcontrol/gangs/territory/TerritoryController.java
@@ -1,5 +1,25 @@
 package net.mcviral.dev.plugins.pvpcontrol.gangs.territory;
 
-public class TerritoryController {
+import java.util.LinkedList;
 
+import net.mcviral.dev.plugins.pvpcontrol.pvp.PVPController;
+
+public class TerritoryController {
+	
+	@SuppressWarnings("unused")
+	private PVPController plugin = null;
+	private LinkedList <Territory> territories = new LinkedList <Territory> ();
+	
+	public TerritoryController(PVPController plugin){
+		this.plugin = plugin;
+	}
+
+	public LinkedList <Territory> getTerritories() {
+		return territories;
+	}
+
+	public void setTerritories(LinkedList <Territory> territories) {
+		this.territories = territories;
+	}
+	
 }
